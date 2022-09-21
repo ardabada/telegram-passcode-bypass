785e16ec09c90c44ce9d1d8bb74989370cf28474
Alexandr Ivanov <arlert@mail.ru>
2022-09-22 01:13:10 +0300

    x86 x64 adjustments

--- Injector/Program.cs
+++ Injector/Program.cs
@@ -17,7 +17,7 @@ namespace Injector
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Running is {0} mode.", Is32Bit ? "x86" : "x64");
+            Console.WriteLine("Running in {0} mode.", Is32Bit ? "x86" : "x64");
 
             if (!TryGetProcessId(args, out int pid))
             {
@@ -41,6 +41,7 @@ namespace Injector
                 return;
             }
 
+            bool is64BitProcess = Is64Bit(process);
             if (Is32Bit && Is64Bit(process))
             {
                 Console.WriteLine("A 32 bit processes cannot access modules of a 64 bit process.");
@@ -58,7 +59,7 @@ namespace Injector
 
             Console.WriteLine("Located main module {0} of process {1} ({2}) on address 0x{3}", module.ModuleName, process.ProcessName, process.Id, module.BaseAddress.ToString("x8"));
 
-            InjectBypass(process.Id, module.BaseAddress.ToInt64(), mode.Value);
+            InjectBypass(process.Id, is64BitProcess, module.BaseAddress.ToInt64(), mode.Value);
         }
 
         private static bool TryGetProcessId(string[] args, out int pid)
@@ -108,9 +109,10 @@ namespace Injector
             return true;
         }
 
-        private static void InjectBypass(int processId, long baseAddress, PasscodeMode mode)
+        private static void InjectBypass(int processId, bool is64BitProcess, long baseAddress, PasscodeMode mode)
         {
-            var target = (IntPtr)(baseAddress + (Is32Bit ? Offset32 : Offset64));
+            Console.WriteLine("Target process running in {0} mode.", is64BitProcess ? "x64" : "x86");
+            var target = (IntPtr)(baseAddress + (is64BitProcess ? Offset64 : Offset32));
             byte[] replacement = OPCODE_JNS;
             if (mode == PasscodeMode.AllowOnlyValid) replacement = OPCODE_JNZ;
             else if (mode == PasscodeMode.AllowOnlyInvalid) replacement = OPCODE_JZ;
